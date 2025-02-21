c59ddd4cd6df7f7e3ee9da2d9344f49af7a40e02
Shailesh kumar sahu <93207540+shailesh-sahu@users.noreply.github.com>
2025-02-21 20:49:50 +0530

    Update vite.config.js
    changed the api for jobs server

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port:3000,
     proxy:{
       '/api':{
-        target: 'http://localhost:8000',
+        target: 'https://shailesh-sahu.github.io/JOb/jobs.json',
         changeOrigin:true,
         rewrite: (path)=> path.replace(/^\/api/,''),
       }
