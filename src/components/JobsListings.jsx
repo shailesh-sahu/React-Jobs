ab363bdbc79c20c8f5734368aa4fb05e860fc451
Shailesh kumar sahu <93207540+shailesh-sahu@users.noreply.github.com>
2025-02-24 12:12:13 +0530

    Update JobsListings.jsx
    made it to fetch data locally

--- src/components/JobsListings.jsx
+++ src/components/JobsListings.jsx
@@ -1,30 +1,10 @@
 import React from 'react'
 import JobListing from './JobListing'
-import {useState, useEffect} from 'react'
-import Spinner from './Spinner'
-const JobsListings = ({ isHome = false }) => {
-  const[jobs,setJobs]=useState([]);
-  const[loading,setLoading]=useState(true);
-useEffect( ()=> {
-  const fetchJobs= async () => {
-    const apiUrl = isHome ? '/api/jobs?_limit=3' : '/api/jobs' ;
-    try {
-      const res = await fetch(apiUrl);
-    const data = await res.json();
-    setJobs(data);
-    } catch (error) {
-      console.log('Error fetching data',error);
-    }
-    finally{
-      setLoading(false);
-    }
-    
-  }
-  fetchJobs();
-}, [] )
+import jobs from '../jobs.json'
 
-  // const rj= isHome ? jobs.slice(0,3) :jobs;
- 
+  
+  const JobsListings = ({ isHome = false }) => {
+    const jobListings= isHome ? jobs.slice(0,3) :jobs;
   return (
     
     <section className="bg-blue-50 px-4 py-10">
@@ -33,17 +13,13 @@ useEffect( ()=> {
         {isHome ? 'Recent Jobs' : 'All Jobs'}
       </h2>
       
-        {loading ? (<Spinner loading={loading}/>) :(
+        {
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-             {jobs.map((job)=> (
+             {jobListings.map((job)=> (
               <JobListing key={job.id} job={job} />
         ))}
         </div>
-        ) }
- 
-        
-      
-     
+         }
     </div>
   </section>
   )
